06bb9efcb26cea950297237bf77228e12786df19
David Farr <david.farr@sap.com>
2015-09-17 13:55:40 -0700

    Update

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,8 +1,7 @@
-require 'compass'
-require 'sass'
-require 'uglifier'
+require "jekyll-assets"
 
-require 'jekyll-assets'
-require 'jekyll-assets/compass'
-require 'jekyll-assets/bootstrap'
-require 'jekyll-assets/font-awesome'
+# bootstrap requires minimum precision of 10, see https://github.com/twbs/bootstrap-sass/issues/409
+::Sass::Script::Number.precision = [10, ::Sass::Script::Number.precision].max
+
+require "jekyll-assets/bootstrap"
+require "jekyll-assets/font-awesome"
